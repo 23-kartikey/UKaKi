6c1644d3d46e1a3821da87cdc3651457f0e8db5d
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-03 23:01:58 +0530

    Initial commit: UKaKi base project

---
+++ src/main/java/com/UKaKi/ukaki/UkakiApplication.java
@@ -0,0 +1,13 @@
+package com.UKaKi.ukaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkakiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkakiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UKaKi/ukaki/UkakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UKaKi.ukaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
