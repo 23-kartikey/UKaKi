60ae3291de5946c5a122c536106da7ac8afb1bd6
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-05 18:41:45 +0530

    feat: created the controller filefor notes

---
+++ src/main/java/com/UKaKi/ukaki/controller/NoteController.java
@@ -0,0 +1,12 @@
+package com.UKaKi.ukaki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/notes")
+@Controller
+public class NoteController {
+    
+
+
+}
