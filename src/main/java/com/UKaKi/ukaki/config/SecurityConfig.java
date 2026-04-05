ef1c63cc3df8c0429c1180141f6eca8d001133a4
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-05 17:43:45 +0530

    feat: created SecurityFilterChain and allowed home page to be accessible by everyone

--- src/main/java/com/UKaKi/ukaki/config/SecurityConfig.java
+++ src/main/java/com/UKaKi/ukaki/config/SecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -15,6 +17,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        return http
+            .authorizeHttpRequests(auth->auth
+                .requestMatchers("/","/**").permitAll()
+            )
+            .formLogin(login->login
+                .loginPage("/login")
+            )
+            .build();
     }
 
     @Bean
