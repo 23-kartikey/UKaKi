101e0146f522d4f411558757f625863ff1442e16
Kartikey Chauhan <kaarrttiikkeey@gmail.com>
2026-04-07 23:25:33 +0530

    feat: configured GetMapping to get recent notes

--- src/main/java/com/UKaKi/ukaki/controller/NoteController.java
+++ src/main/java/com/UKaKi/ukaki/controller/NoteController.java
@@ -1,12 +1,29 @@
 package com.UKaKi.ukaki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/notes")
-@Controller
+import com.UKaKi.ukaki.data.NoteRepository;
+import com.UKaKi.ukaki.entity.Note;
+
+@RequestMapping(path="/notes", produces="application/json")
+@RestController
 public class NoteController {
     
+    private final NoteRepository noteRepo;
 
+    public NoteController(NoteRepository noteRepo){
+        this.noteRepo=noteRepo;
+    }
+
+    @GetMapping(params="recent")
+    public Iterable<Note> recentNotes(){
+        PageRequest page= PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        return noteRepo.findAll(page).getContent();
+    }
 
 }
